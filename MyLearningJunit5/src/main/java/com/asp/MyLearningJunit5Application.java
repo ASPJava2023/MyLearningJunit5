a235c6aaafbdd4a823945b03a74f47ff844ae2d7
ASPJava2023 <ankit.singh.pawar.newlaptop@gmail.com>
2025-07-20 00:26:23 +0530

    First commit

---
+++ MyLearningJunit5/src/main/java/com/asp/MyLearningJunit5Application.java
@@ -0,0 +1,13 @@
+package com.asp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLearningJunit5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLearningJunit5Application.class, args);
+	}
+
+}
---
+++ MyLearningJunit5/src/test/java/com/asp/MyLearningJunit5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLearningJunit5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
