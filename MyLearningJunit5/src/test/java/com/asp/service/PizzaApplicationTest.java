a7e8e8f1991bd7e9c6c47db6cb947c885749e4d3
ASPJava2023 <ankit.singh.pawar.newlaptop@gmail.com>
2025-07-20 23:28:04 +0530

    First level is done

--- MyLearningJunit5/src/test/java/com/asp/service/PizzaApplicationTest.java
+++ MyLearningJunit5/src/test/java/com/asp/service/PizzaApplicationTest.java
@@ -1,12 +1,32 @@
 package com.asp.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class PizzaApplicationTest {
-	
+
+	PizzaApplicationService pizzaApplicationService = new PizzaApplicationService();
+
 	@Test
 	public void pizzaonDineTest() {
-		
+		int actualPrice = pizzaApplicationService.pizzaonDine(2, 250);
+		int expectedPrice = 500;
+		assertEquals(actualPrice, expectedPrice);
+	}
+
+	@Test
+	public void pizzaOnDeliveryTest() {
+		int actualPrice = pizzaApplicationService.pizzaOnDelivery(2, 250, 50);
+		int expectedPrice = 550;
+		assertEquals(actualPrice, expectedPrice);
+	}
+
+	@Test
+	public void sundayOfferTest() {
+		int actualPrice = pizzaApplicationService.sundayOffer(2, 250, 20);
+		int expectedPrice = 400;
+		assertEquals(actualPrice, expectedPrice);
 	}
 
 }
