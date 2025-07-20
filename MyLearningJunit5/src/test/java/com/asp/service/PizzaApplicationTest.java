caf27be629a650ac5d4ba0c67024f1d358774c7c
ASPJava2023 <ankit.singh.pawar.newlaptop@gmail.com>
2025-07-21 00:05:52 +0530

    Learned Ordering the Testcases

--- MyLearningJunit5/src/test/java/com/asp/service/PizzaApplicationTest.java
+++ MyLearningJunit5/src/test/java/com/asp/service/PizzaApplicationTest.java
@@ -4,16 +4,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
+import org.junit.jupiter.api.ClassOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+@DisplayName("Test For Pizza Application")
+//@TestMethodOrder(OrderAnnotation.class)
 public class PizzaApplicationTest {
 
 	PizzaApplicationService pizzaApplicationService = new PizzaApplicationService();
 	
 	@DisplayName("Test for  pizzaonDine")
 	@Test
+	@Order(value = 2)
 	public void pizzaonDineTest() {
 		int actualPrice = pizzaApplicationService.pizzaonDine(2, 250);
 		int expectedPrice = 500;
@@ -22,6 +28,7 @@ public class PizzaApplicationTest {
 	
 	@DisplayName("Test for pizzaOnDelivery")
 	@Test
+	@Order(value = 3)
 	public void pizzaOnDeliveryTest() {
 		int actualPrice = pizzaApplicationService.pizzaOnDelivery(2, 250, 50);
 		int expectedPrice = 550;
@@ -29,20 +36,24 @@ public class PizzaApplicationTest {
 	}
 
 	@Test
+	@Order(value = 4)
 	public void sundayOfferTest() {
 		int actualPrice = pizzaApplicationService.sundayOffer(2, 250, 20);
 		int expectedPrice = 400;
 		assertEquals(actualPrice, expectedPrice);
 	}
 	
+	@Order(value = 5)
 	@DisplayName("Test for Getting Env varriable")
 	@Test
-	@Disabled
+	@Disabled("Custom Env is not set as of Now")
 	public void envTest() {
 		assertTrue(System.getenv("ENV").startsWith("D"));
 	}
+	
 	@DisplayName(" Test to learn Assume")
 	@Test
+	@Order(value = 1)
 	public void partyForDeveloperTeamTest() {
 		assumeTrue("Dev".equals(System.getenv("ENV")),
 				()->"Not from Developer Team");
